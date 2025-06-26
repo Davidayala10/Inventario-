b1887ecc95c9fb53a0f170fd050bcb9c12b5f759
Davidayala10 <deiiviid622@gmail.com>
2025-06-25 21:41:29 -0600

    Ajustes en Dockerizacion

--- inventariobackend/inventariobackend/settings.py
+++ inventariobackend/inventariobackend/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 
-'corsheaders.middleware.CorsMiddleware',    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',    
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +131,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
